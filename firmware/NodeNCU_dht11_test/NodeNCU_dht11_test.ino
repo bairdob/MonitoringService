858badc1ce06296d9ebf7c8a6cd8e5de23bc646d
bairdob <krumane@mail.ru>
2022-09-11 22:22:56 +0300

    fix: Upload correct version firmware

--- firmware/NodeNCU_dht11_test/NodeNCU_dht11_test.ino
+++ firmware/NodeNCU_dht11_test/NodeNCU_dht11_test.ino
@@ -1,42 +1,156 @@
-//This example for use ESP8266 with Adafruit DHT11 library: 
-//https://github.com/adafruit/DHT-sensor-library
 #include "DHT.h"
-// connect data pin of DHT11 to D2 ESP8266 NodeMCU
-#define DHTPIN D4    
+#include <ESP8266WiFi.h>
+#include <Ticker.h>
+#include <AsyncMqttClient.h>
 
-// Uncomment whatever type you're using!
+#define WIFI_SSID "Keenetic-3556"
+#define WIFI_PASSWORD "bair457589"
+
+// Raspberri Pi Mosquitto MQTT Broker
+#define MQTT_HOST IPAddress(192, 168, 1, 39)
+// For a cloud MQTT broker, type the domain name
+//#define MQTT_HOST "broker.emqx.io"
+#define MQTT_PORT 1883
+
+// Temperature MQTT Topics
+#define MQTT_PUB_TEMP "/flask/mqtt"
+#define MQTT_PUB_HUM "/flask/mqtt"
+#define MQTT_PUB_TOPIC "/flask/mqtt"
+
+// Digital pin connected to the DHT sensor
+#define DHTPIN D4  
+
+// Uncomment whatever DHT sensor type you're using
 #define DHTTYPE DHT11   // DHT 11
 //#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
-// Connect pin 1 (on the left) of the sensor to +5V
-// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
-// to 3.3V instead of 5V!
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
-
+// Initialize DHT sensor
 DHT dht(DHTPIN, DHTTYPE);
-bool led_status=false;
+
+// Variables to hold sensor readings
+float temp;
+float hum;
+
+// Variables to hold JSON
+String JSON_start = "{ \"devices\": [{ \"mac\": \"MAC\", \"name\": \"305\", \"sensors\": [{ \"id\": \"T1\", \"value\": ";
+String JSON_end = ", \"unit\": \"%\"} ]} ]}";
+String JSON_result = "";
+
+AsyncMqttClient mqttClient;
+Ticker mqttReconnectTimer;
+
+WiFiEventHandler wifiConnectHandler;
+WiFiEventHandler wifiDisconnectHandler;
+Ticker wifiReconnectTimer;
+
+unsigned long previousMillis = 0;   // Stores last time temperature was published
+const long interval = 1000;        // Interval at which to publish sensor readings
+
+void connectToWifi() {
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+}
+
+void onWifiConnect(const WiFiEventStationModeGotIP& event) {
+  Serial.println("Connected to Wi-Fi.");
+  connectToMqtt();
+}
+
+void onWifiDisconnect(const WiFiEventStationModeDisconnected& event) {
+  Serial.println("Disconnected from Wi-Fi.");
+  mqttReconnectTimer.detach(); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+  wifiReconnectTimer.once(2, connectToWifi);
+}
+
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+}
+
+void onMqttConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  Serial.println("Disconnected from MQTT.");
+
+  if (WiFi.isConnected()) {
+    mqttReconnectTimer.once(2, connectToMqtt);
+  }
+}
+
+/*void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
+  Serial.println("Subscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+  Serial.print("  qos: ");
+  Serial.println(qos);
+}
+
+void onMqttUnsubscribe(uint16_t packetId) {
+  Serial.println("Unsubscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}*/
+
+void onMqttPublish(uint16_t packetId) {
+  Serial.print("Publish acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
+  Serial.println();
+
   dht.begin();
+  
+  wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
+  wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
+
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onDisconnect(onMqttDisconnect);
+  //mqttClient.onSubscribe(onMqttSubscribe);
+  //mqttClient.onUnsubscribe(onMqttUnsubscribe);
+  mqttClient.onPublish(onMqttPublish);
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+  // If your broker requires authentication (username and password), set them below
+  //mqttClient.setCredentials("REPlACE_WITH_YOUR_USER", "REPLACE_WITH_YOUR_PASSWORD");
+  
+  connectToWifi();
 }
 
 void loop() {
-  delay(2000);
-  led_status=!led_status;
+  unsigned long currentMillis = millis();
+  // Every X number of seconds (interval = 10 seconds) 
+  // it publishes a new MQTT message
+  if (currentMillis - previousMillis >= interval) {
+    // Save the last time a new reading was published
+    previousMillis = currentMillis;
+    // New DHT sensor readings
+    hum = dht.readHumidity();
+    // Read temperature as Celsius (the default)
+    temp = dht.readTemperature();
+    // Read temperature as Fahrenheit (isFahrenheit = true)
+    //temp = dht.readTemperature(true);
+    
+    // Publish an MQTT message on topic esp/dht/temperature
+//    uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_TEMP, 1, true, String(temp).c_str());                            
+//    Serial.printf("Publishing on topic %s at QoS 1, packetId: %i ", MQTT_PUB_TEMP, packetIdPub1);
+//    Serial.printf("Message: %.2f \n", temp);
 
-  float h = dht.readHumidity();     // get humidity
-  float t = dht.readTemperature();  // get temperature
-  if ( !isnan(h) && !isnan(t)) {
-      Serial.print("Humidity: ");
-      Serial.print(h);
-      Serial.print(" %  Temperature: ");
-      Serial.print(t);
-      Serial.println(" *C ");
+    // Publish an MQTT message on topic esp/dht/humidity
+//    uint16_t packetIdPub2 = mqttClient.publish(MQTT_PUB_HUM, 1, true, String(hum).c_str());                            
+//    Serial.printf("Publishing on topic %s at QoS 1, packetId %i: ", MQTT_PUB_HUM, packetIdPub2);
+//    Serial.printf("Message: %.2f \n", hum);
+    
+    // Publish an MQTT message JSON
+    JSON_result = JSON_start + String(hum) + JSON_end;
+    uint16_t packetIdPub3 = mqttClient.publish(MQTT_PUB_TOPIC, 1, true, JSON_result.c_str());
+    Serial.printf("{ \"devices\": [{ \"mac\": \"MAC\", \"name\": \"305\", \"sensors\": [{ \"id\": \"T1\", \"value\": %.2f, \"unit\": \"°C\"} ]} ]}\n", hum);
+
   }
-  digitalWrite(LED_BUILTIN,led_status);
 }
